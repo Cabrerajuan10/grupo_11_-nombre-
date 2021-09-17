85aff2c8f326fdac48a2fad8147db7ad11d0d0a4
MatiB-Git <matiasbuccione@outlook.com>
2021-09-17 06:39:09 -0300

    se agrego carpeta data, utils y srcipts en partials, se creo la vista admin, se agregaron rutas y se implemento el crud de productos

--- sitio/routes/index.js
+++ sitio/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-const {home} = require('../controllers/indexController')
+const {home, admin} = require('../controllers/indexController')
 
 /* GET home page. */
-router.get('/',home);
+router.get('/', home);
+router.get('/admin', admin)
 
 module.exports = router;
--- sitio/routes/users.js
+++ sitio/routes/users.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-const {register,login} = require ('../controllers/usersController')
+const {register, login} = require ('../controllers/usersController')
 
 /* GET users listing. */
-router.get('/register',register);
-router.get('/login',login);
+router.get('/register', register);
+router.get('/login', login);
 
 module.exports = router;
