1e765e0d50a7530a0fc3bb5f34dffafbc551bc8e
formasmaxigit <maximnt9583@gmail.com>
2021-09-17 09:29:05 -0300

    ignorar cambios

--- sitio/controllers/indexController.js
+++ sitio/controllers/indexController.js
@@ -1,8 +1,8 @@
 const path = require ('path');
 module.exports = {
-    /* home : (req,res) => res.render(path.join(__dirname,'..','views','home')), */
+   /* home : (req,res) => res.render(path.join(__dirname,'..','views','home')),*/
 
-    home : (req,res) =>{
+  home : (req,res) =>{
         return res.render('home')
     },
     
--- sprint2/app.js
+++ sprint2/app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const port = 3030;
 
+
+/* Recursos estaticos */
 app.use(express.static('public'));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','home.html')));
