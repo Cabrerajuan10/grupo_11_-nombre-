b82fc12a54fdd54829f458a63383293546c6eff6
MatiB-Git <matiasbuccione@outlook.com>
2021-09-19 05:51:37 -0300

    se corrigio estructura y links en detailProduct

--- sitio/public/javascripts/detailProduct.js
+++ sitio/public/javascripts/detailProduct.js
@@ -1,5 +1,5 @@
 /*SLIDER PRODUTOS RECOMENDADOS*/
-new Glider(document.querySelector('.glider'), {
+new Glider(document.querySelector('.glider-detail'), {
         // Mobile-first defaults
         slidesToShow: 'auto',
         slidesToScroll: 1,
@@ -26,7 +26,7 @@ new Glider(document.querySelector('.glider'), {
             breakpoint: 769,
             settings: {
               // Set to `auto` and provide item width to adjust to viewport
-              slidesToShow: 3,
+              slidesToShow: '3',
               slidesToScroll: 'auto',
               itemWidth: 150,
               duration: 0.25
@@ -50,17 +50,17 @@ new Glider(document.querySelector('.glider'), {
 
 
 document.getElementById('img-prev1').addEventListener('click', () =>{
-        document.getElementById('img-principal').setAttribute('src', '/img/detailProduct/smartv-43-samsung-img1.png')
+        document.getElementById('img-principal').setAttribute('src', '/images/detailProduct/smartv-43-samsung-img1.png')
 })
 
 
 document.getElementById('img-prev2').addEventListener('click', () =>{
-        document.getElementById('img-principal').setAttribute('src', 'img/detailProduct/smartv-43-samsung-img2.png')
+        document.getElementById('img-principal').setAttribute('src', '/images/detailProduct/smartv-43-samsung-img2.png')
 })
 
 
 document.getElementById('img-prev3').addEventListener('click', () =>{
-        document.getElementById('img-principal').setAttribute('src', '/img/detailProduct/smartv-43-samsung-img3.png')
+        document.getElementById('img-principal').setAttribute('src', '/images/detailProduct/smartv-43-samsung-img3.png')
 })
 
 
