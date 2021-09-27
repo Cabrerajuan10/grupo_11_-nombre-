82644e6e25808882fe4b239cf71afd17033df72b
MatiB-Git <matiasbuccione@outlook.com>
2021-09-27 08:06:08 -0300

    se configuro funcionalidad para subir mas de una imegen por producto

--- sitio/controllers/usersController.js
+++ sitio/controllers/usersController.js
@@ -3,10 +3,14 @@ const path = require ('path');
 module.exports = {
 
     register : (req,res) =>{
-        return res.render('register')
+        return res.render('register', {
+            title: "Registro"
+        })
     },
 
     login : (req,res) =>{
-        return res.render('login')
+        return res.render('login', {
+            title: "Login"
+        })
     }
 }
