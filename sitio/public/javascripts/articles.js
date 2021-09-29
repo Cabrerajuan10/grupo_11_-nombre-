c727885c4ac122a3c6923b0db04aa4c49f5acdad
MatiB-Git <matiasbuccione@outlook.com>
2021-09-29 11:24:56 -0300

    se hicieron correciones en las tarjetas de los productos

--- sitio/public/javascripts/articles.js
+++ sitio/public/javascripts/articles.js
@@ -1,33 +1,45 @@
+/*SLIDER PRODUTOS RECOMENDADOS*/
 new Glider(document.querySelector('.glider1'), {
-    // Mobile-first defaults
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    scrollLock: true,
-    dots: '#dots1',
-    arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-    },
-    responsive: [
-      {
-        // screens greater than >= 775px
-        breakpoint: 775,
-        settings: {
-          // Set to `auto` and provide item width to adjust to viewport
-          slidesToShow: 3,
-          slidesToScroll: 2,
-          itemWidth: 150,
-          duration: 0.25
-        }
-      },{
-        // screens greater than >= 1024px
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 2,
-          itemWidth: 150,
-          duration: 0.25
-        }
+  // Mobile-first defaults
+  slidesToShow: 'auto',
+  slidesToScroll: 1,
+  scrollLock: true,
+  dots: 'dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  },
+  responsive: [
+          {
+       // screens greater than >= 775px
+        breakpoint: 480,
+       settings: {
+         // Set to `auto` and provide item width to adjust to viewport
+         slidesToShow: '2',
+         slidesToScroll: 'auto',
+        itemWidth: 150,
+       duration: 0.25
       }
-    ]
-  });
+   },
+             {
+      // screens greater than >= 775px
+      breakpoint: 769,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: '3',
+        slidesToScroll: 'auto',
+        itemWidth: 150,
+        duration: 0.25
+      }
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: '4',
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25
+      }
+    }
+  ]
+});
