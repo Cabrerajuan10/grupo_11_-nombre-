bec3fe64a204fc753816c9a136e6643547541fd6
MatiB-Git <matiasbuccione@outlook.com>
2021-12-19 12:02:56 -0300

    se agregaron productos al seeder

--- sitio/database/seeders/20211110052228-create-products.js
+++ sitio/database/seeders/20211110052228-create-products.js
@@ -83,12 +83,82 @@
     createdAt: new Date
 },
 {
-    "name": "Equipo de musica - Sony",
-    "description": "pede posuere nonummy integer non velit donec",
-    "price": 28.999,
-    "priceRegular": 350000,
-    "discount": 10,
-    "categoryId": 3,
+    "name": "Aire acondicionado Philco 2200F",
+    "description": "El aire Philco PHS25HA3AN enfría de manera rápida tus ambientes, gracias a los deflectores de aire y el ventilador que opera a sus máximas capacidades. Además, se caracteriza por su bajo nivel sonoro, debido a que los equipos tienen compresores y ventiladores de última generación, para asegurar un funcionamiento silencioso. Ofrece control remoto y múltiples funciones.",
+    "price": 41.999,
+    "priceRegular": 55.000,
+    "discount": 0,
+    "categoryId": 5,
+    "show": true,
+    createdAt: new Date
+},
+{
+    "name": "Heladera Cíclica Patrick HPK141M10S 364Lt",
+    "description": "La heladera Patrick HPK141M10S cuenta con un moderno y cómodo diseño con el freezer en la parte superior y el refrigerador en la inferior. En su interior, viene con anaqueles y estantes regulables en altura que se adaptan a las diversas necesidades de guardado de productos y alimentos. Además, trae un cajón especial para frutas y verduras. Por otro lado, cuenta con un dispenser de agua incorporado en la puerta para acceder fácilmente con capacidad para 2 litros.",
+    "price": 75.999,
+    "priceRegular": 89.000,
+    "discount": 0,
+    "categoryId": 1,
+    "show": true,
+    createdAt: new Date
+},
+{
+    "name": "Lavarropas Samsung 9kg 1400 RPM",
+    "description": "El lavarropas Samsung utiliza el sistema motor Digital Inverter que trabaja sin cepillos y contiene pequeñas partes que se mueven independientemente, así hay menos probabilidad de que se desgasten o rompan y garantizan una vida más larga. Por otro lado, permite programar el final del ciclo para determinada cantidad de horas posteriores con su botón especial de Final diferido.",
+    "price": 97.999,
+    "priceRegular": 105.999,
+    "discount": 0,
+    "categoryId": 1,
+    "show": true,
+    createdAt: new Date
+},
+{
+    "name": "Cocina Whirlpool Multigas WFX57DI 56cm",
+    "description": "La cocina Whirlpool WFX57DI posee un diseño clásico en color Silver que cuenta con cuatro hornallas, horno y cajón parrilla independiente. Dispone de 1 quemador grande, 2 medianos y uno pequeño. A su vez, su construcción hermética evita que se filtre suciedad o líquidos previniendo desgaste y brindando mayor seguridad.",
+    "price": 89.999,
+    "priceRegular": 94.999,
+    "discount": 0,
+    "categoryId": 1,
+    "show": true,
+    createdAt: new Date
+},
+{
+    "name": "Microondas Atma 700W 20Lt",
+    "description": "El microondas Atma MD1720N tiene un diseño clásico en color blanco. Viene con bandeja giratoria y ventana en la puerta para poder controlar las comidas. Asimismo, posee un panel de control rotativo muy práctico y fácil de usar, donde se encuentran las diferentes funciones.",
+    "price": 16.499,
+    "priceRegular": 24.999,
+    "discount": 0,
+    "categoryId": 1,
+    "show": true,
+    createdAt: new Date
+},
+{
+    "name": "Pava Eléctrica Philips HD9368 90",
+    "description": "Con una capacidad para 1,7 litros de agua, la pava eléctrica Philips HD9368 90 es ideal para cualquier cocina. Elegante y con cuidados detalles de diseño, viene con un mango ergonómico, soportes antideslizantes y una base plana de 360 grados. Su tapa con bisagra y abertura amplia permite llenar y limpiar fácilmente su interior",
+    "price": 4.999,
+    "priceRegular": 6.999,
+    "discount": 0,
+    "categoryId": 1,
+    "show": true,
+    createdAt: new Date
+},
+{
+    "name": "Xbox Series X Microsoft 1 TB",
+    "description": "La Xbox Series X está completamente renovada y presenta un gran salto generacional respecto de la Xbox One. Esta versión te permite nuevos niveles de velocidad y rendimiento. En el corazón de la Serie X se encuentra la Xbox Velocity Architecture, que combina una unidad SSD personalizada y software integrado para disminuir significativamente los tiempos de carga dentro y fuera del juego. Simultáneamente podés moverte sin problemas entre varios juegos en un instante con Quick Resume.",
+    "price": 169.999,
+    "priceRegular": 179.999,
+    "discount": 0,
+    "categoryId": 1,
+    "show": true,
+    createdAt: new Date
+},
+{
+    "name": "Celular Samsung A01 Core 16GB Azul",
+    "description": "Posee una memoria interna ROM de 16 GB mientras que la externa se puede expandir mediante una tarjeta micro SD hasta 512 GB. Vas a poder almacenar videos, fotos y música sin problemas.El celular Samsung A01 Core presenta un diseño ergonómico y elegante. Con la pantalla HD de 5,3 pulgadas con una resolución de 1480 x 720 píxeles vas a poder sumergirte en tus juegos y videos favoritos",
+    "price": 15.999,
+    "priceRegular": 19.999,
+    "discount": 0,
+    "categoryId": 4,
     "show": true,
     createdAt: new Date
 }
